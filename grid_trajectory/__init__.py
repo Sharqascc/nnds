d85fc9b97ae15be5fbbc5937bdd2633d9441422b
NNDS Patcher <colab@nnds.com>
2026-04-30 03:58:10 +0000

    fix: Critical patches - BEVMapper, grid imports, SAM3 handling, research_run logging

--- grid_trajectory/__init__.py
+++ grid_trajectory/__init__.py
@@ -1,12 +1,8 @@
 # grid_trajectory package
 
-# Import main functions that actually exist
 from .pet_grid import TrajectoryLogger, compute_pet
 from .spatial_grid import SpatialGrid
 from .sam3_grid_pet import run_sam3_grid_pet
-
-# These functions might not exist yet - comment them out
-# from .sam3_grid_pet import build_grid_trajectories, compute_pet_from_tracks
 
 __all__ = [
     'TrajectoryLogger',
