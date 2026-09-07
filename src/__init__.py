a80117dd5b265b883e5123f18c7caedda176b21f
sharqascc <sharqascc@users.noreply.github.com>
2026-09-07 02:41:44 +0000

    Add deterministic seeding at package import

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""Package initialization for src."""
+
+import os
+
+from src.utils.seed import set_seed
+
+# Use environment variable GLOBAL_SEED if set, otherwise default to 42
+_global_seed = int(os.getenv("GLOBAL_SEED", "42"))
+set_seed(_global_seed)
