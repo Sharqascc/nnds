aa814cdc3086c328e0d7357df4213f5bc4ab50c3
sharqascc <sharqascc@users.noreply.github.com>
2026-09-07 17:01:09 +0000

    Add harmless comment to test agentic workflow

--- src/__init__.py
+++ src/__init__.py
@@ -7,3 +7,5 @@ from src.utils.seed import set_seed
 # Use environment variable GLOBAL_SEED if set, otherwise default to 42
 _global_seed = int(os.getenv("GLOBAL_SEED", "42"))
 set_seed(_global_seed)
+
+# Agentic test change
